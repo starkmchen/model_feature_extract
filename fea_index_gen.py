5eb1f87a7dbd685c741aa1d4cf7364256e48f788
starkmchen <ll3021359>
2021-03-15 16:20:51 +0000

    add feature index

---
+++ fea_index_gen.py
@@ -0,0 +1,28 @@
+import sys
+from collections import defaultdict
+
+
+def main(argv):
+  feature_count = defaultdict(set)
+  f = open(argv[0])
+  fw = open(argv[1], 'w')
+  for line in f:
+    fea_k, fea_v, click, install, imp = line.strip().split('\t') 
+    feature_count[fea_k].add(fea_v)
+  for fea_k, v in feature_count.iteritems():
+    if len(v) == 1:
+      continue
+    if fea_k == "up_uid":
+      fea_cnt = 200000
+    elif fea_k == "ctx_ip":
+      fea_cnt = 100000
+    else:
+      fea_cnt = len(v) * 10
+    output = [fea_k, 0, fea_cnt, 'int', 0, 0, 0]
+    fw.write('\t'.join(map(str, output)) + '\n')
+    
+
+
+
+if __name__ == '__main__':
+  main(sys.argv[1:])
