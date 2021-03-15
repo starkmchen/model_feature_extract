e0fc692ba0ab6d62f0fdd1accc84cf32a1459161
starkmchen <ll3021359>
2021-03-15 12:43:36 +0000

    add make.sh

---
+++ make.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+rm -r proto
+cp -r ../proto .
+./protoc -I=. --cpp_out=proto/ proto/*.proto
+mv proto/proto/* proto
+
+#g++ -o ad_feature.so --shared -fPIC -I/mnt/chenmin/ad_feature -I/mnt/chenmin/lib/include ads_feature.cpp hash.cpp ../proto/*.cc -L/mnt/chenmin/lib/lib/ -Wl,-rpath=/mnt/chenmin/lib/lib -lprotobuf --std=c++11
+#g++ -o main -fPIC -I/mnt/chenmin/ad_feature -I/mnt/chenmin/lib/include ads_feature.cpp hash.cpp ../proto/*.cc main.cpp /mnt/chenmin/lib/lib/libprotobuf.a -lpthread --std=c++11
+#g++ -o libpyfeature_extract.so --shared -fPIC -I/usr/include/python2.7 -I/mnt/chenmin/ad_feature -I/mnt/chenmin/lib/include ads_feature.cpp hash.cpp pybind.cpp ../proto/*.cc -L/mnt/chenmin/lib/lib/ -Wl,-rpath=/mnt/chenmin/lib/lib -lprotobuf --std=c++11
+#g++ -shared -fPIC -o libpyfeature_extract.so -I/usr/include/python2.7 -I/mnt/chenmin/ad_feature -I/mnt/chenmin/lib/include ads_feature.cpp hash.cpp pybind.cpp ../proto/*.cc -Wl,-whole-archive /mnt/chenmin/lib/lib/libprotobuf.a -Wl,-no-whole-archive --std=c++11
+
+g++ -o libpyfeature_extract.so --shared -fPIC -I/usr/include/python2.7 -I/mnt/chenmin/ad_feature -I/mnt/chenmin/ad_feature/proto -I/mnt/chenmin/lib/include ads_feature.cpp hash.cpp pybind.cpp ../proto/*.cc -L/mnt/chenmin/lib/lib/ -Wl,-rpath=lib -lprotobuf --std=c++11
+
