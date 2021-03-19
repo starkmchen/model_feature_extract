41fd235a535e78b046b2548cee4345836f508fd3
starkmchen <ll3021359>
2021-03-19 09:02:31 +0000

    ch make.s

--- make.sh
+++ make.sh
@@ -3,5 +3,7 @@ set -x
 
 cwd=$(pwd)
 
+./protoc -I./ --cpp_out=./ proto/*.proto
+
 g++ -o libpyfeature_extract.so --shared -fPIC -I/usr/include/python2.7 -I${cwd}/src -I${cwd} -I${cwd}/proto -I/mnt/chenmin/lib/include src/ads_feature.cpp pybind.cpp proto/*.cc -L/mnt/chenmin/lib/lib/ -Wl,-rpath=lib -lprotobuf --std=c++11
 
